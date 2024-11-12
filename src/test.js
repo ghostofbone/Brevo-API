a13c0ac414b310a60ed392ee0b106089f99284f3
ghostofbone <140453535+ghostofbone@users.noreply.github.com>
2024-11-12 20:55:49 +0100

    Update test.js

--- src/test.js
+++ src/test.js
@@ -1,38 +1,3 @@
-/*
-const Brevo = require('@getbrevo/brevo');
-
-
-
-// Initialize the API client
-let defaultClient = Brevo.ApiClient.instance;
-
-// Set your API key
-let apiKey = defaultClient.authentications['api-key'];
-apiKey.apiKey = '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';
-
-const apiInstance = new Brevo.ContactsApi(defaultClient);
-
-
-let createContact = new Brevo.CreateContact({
-    email: 'bukhantcovk@gmail.com',
-// Replace with the user's email
-    attributes: {FIRSTNAME: "First", LASTNAME: "Last"},
-// Optional attributes
-    listIds: [2]
-// Replace with your desired list ID
-});
-
-apiInstance.createContact(createContact)
-    .then(data => {
-        console.log('Contact added successfully:', data);
-    })
-    .catch(error => {
-        console.error('Error adding contact:', error);
-    });
-*/
-
-
-
 const axios = require('axios');
 
 const fakeData = [
